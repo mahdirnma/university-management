9bf15130793778706178b58eff439d405048dbbb
mahdirnma <rahnamamahdi78@gmail.com>
2025-01-09 13:25:06 +0330

    create students dashboard

---
+++ resources/views/student/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app3')
+@section('title')
+    student panel
+@endsection
+@section('content')
+    <div class="w-full h-[88%] bg-gray-200 flex items-center justify-center">
+        <div class="w-[90%] h-5/6 bg-white rounded-xl pt-3 flex flex-col items-center">
+            <div class="w-[90%] h-1/5 flex justify-end items-center border-b">
+                <h2 class="text-xl">student panel</h2>
+            </div>
+            <div class="w-[90%] h-3/5 flex flex-col justify-center">
+                <div class="w-[90%] h-3/5 flex flex-row-reverse justify-start">
+                </div>
+            </div>
+        </div>
+@endsection
